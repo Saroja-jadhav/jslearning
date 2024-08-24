e955bcdc441d348432803799a00d9e49cefb6e19
Saroja-jadhav <147601779+Saroja-jadhav@users.noreply.github.com>
2024-08-24 15:13:57 +0000

    container config done

---
+++ javascript/variable.js
@@ -0,0 +1,14 @@
+const accountId = 144552
+let accountEmail ="saroja@google.com"
+var accountPassword ="123456"
+accountCity ="ambajogai"
+
+//accountId = 2 // not
+
+accountEmail = "sa@js.com"
+accountPassword = "12121212"
+accountCity ="latur"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
