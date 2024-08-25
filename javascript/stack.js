bd69f2677475e4f943b8c866eb0ee45d30693033
Saroja-jadhav <147601779+Saroja-jadhav@users.noreply.github.com>
2024-08-25 11:02:13 +0000

    stack

---
+++ javascript/stack.js
@@ -0,0 +1,17 @@
+let myYoutubename = "sarojajadhav.com"
+let anothername = myYoutubename
+anothername ="saroja"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    eamil: "user@google.com",
+    upi: "user@sjk"
+}
+
+let userTwo =userOne
+
+userTwo.eamil ="saroja@google.com"
+console.log(userOne.eamil);
+console.log(userTwo.eamil);
